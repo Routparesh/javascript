25946a7ff8d095963962f4d3f8d12d4d9b0ff1ea
paresh <routpareshkumar737@gmail.com>
2023-11-03 17:54:06 +0530

    loops in js

---
+++ 04_itterations/1.js
@@ -0,0 +1,35 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+// }
+
+
+// const myArray = ['flash', 'superman','batman']
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+   
+//     console.log(element);
+   
+    
+// }
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index === 5){
+//         console.log(`5 decteed`)
+//         break
+//     }
+//     console.log(index)
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index === 5){
+        console.log(`5 decteed`)
+        continue
+    }
+    console.log(index)
+}
