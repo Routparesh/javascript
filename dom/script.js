3c1b5572ab1d735a62b69b4cb8487cdd05a01de8
paresh <routpareshkumar737@gmail.com>
2023-11-11 01:12:19 +0530

    dom selector

---
+++ dom/script.js
@@ -0,0 +1,9 @@
+var title = document.getElementById('title');
+
+title.innerHTML = 'Paresh';
+title.style.color = 'aquamarine';
+console.log(title.textContent );
+
+var ul = document.querySelector('li:nth-child(2)')
+
+ul.style.color = 'green';
