bc9bfa23dc14408999edd5a1bab78f7743f928e6
paresh <routpareshkumar737@gmail.com>
2023-11-11 15:38:31 +0530

    create element through DOM

--- dom/script.js
+++ dom/script.js
@@ -1,9 +1,28 @@
-var title = document.getElementById('title');
+// var title = document.getElementById('title');
 
-title.innerHTML = 'Paresh';
-title.style.color = 'aquamarine';
-console.log(title.textContent );
+// title.innerHTML = 'Paresh';
+// title.style.color = 'aquamarine';
+// console.log(title.textContent );
 
-var ul = document.querySelector('li:nth-child(2)')
+// var ul = document.querySelector('li:nth-child(2)')
 
-ul.style.color = 'green';
+// ul.style.color = 'green';
+
+const parent = document.querySelector('.parent');
+// console.log(parent);
+// console.log(parent.children[2])
+
+// for (let i = 0 ; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerHTML)   
+// }
+
+// parent.children[2].style.color = 'aqua';
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
+
+// const dayOne = document.querySelector('.day')
+// console.log(dayOne)
+// console.log(dayOne.parentElement.innerText);
+// console.log(dayOne.nextElementSibling)
+
+console.log('NODES:', parent.childNodes)
