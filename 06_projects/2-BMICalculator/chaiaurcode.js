032979453e1e3a0c1a1793f7bfc10871a025efa9
paresh <routpareshkumar737@gmail.com>
2023-11-14 10:52:44 +0530

    modify BMI calculator

--- 06_projects/2-BMICalculator/chaiaurcode.js
+++ 06_projects/2-BMICalculator/chaiaurcode.js
@@ -6,6 +6,7 @@ form.addEventListener('submit', function(e){
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
+    const text = document.querySelector('#text');
 
     if (height === '' || height <= 0 || isNaN(height)){
         results.innerHTML = 'please give a valid height'
@@ -14,5 +15,12 @@ form.addEventListener('submit', function(e){
     }else{
         const bmi = (weight/ ((height*height)/10000)).toFixed(2)
         results.innerHTML = `<span>${bmi}</span>`
+        if(bmi < 18.6){
+            text.innerHTML = 'Under Weight';
+        }else if(bmi > 18.6 && bmi < 24.9){
+            text.innerHTML = 'Normal Range';
+        }else{
+            text.innerHTML = 'Over Weight';
+        }
     }
 })
