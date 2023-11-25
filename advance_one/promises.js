575af5e7e4c26d6b1cf9267b70dc060771d13a32
paresh <routpareshkumar737@gmail.com>
2023-11-25 11:57:43 +0530

    async ,fetch

--- advance_one/promises.js
+++ advance_one/promises.js
@@ -44,11 +44,62 @@ const promiseFour = new Promise(function (resolve, reject) {
     },1000)  
 });
 
-promiseFour.then(function(user){
+promiseFour
+.then(function(user){
     console.log(user)
     return user.username
-}).then(function(username){
+})
+.then(function(username){
     console.log(username)
-}).catch(function(error){
+})
+.catch(function(error){
     console.log(error)
+})
+.finally(function(){
+    console.log('done')
+});
+
+const promiseFive = new Promise(function(resolve, reject){ 
+        setTimeout(function(){
+            let error = true;
+
+        if (!error){
+            resolve({
+                username: 'paresh kumar rout',
+                password: '123'
+            })
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+        },1000)
+})
+
+async function consumePromiseFive(){
+   try {
+    let response = await promiseFive;
+    console.log(response)
+   } catch (error) {
+    console.log(error)
+   }
+}
+
+consumePromiseFive()
+
+// async function getUsers(){
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// getUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
 })
